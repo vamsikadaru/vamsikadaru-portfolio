ede0102baf3c548ab6774bdab88068df87363f11
Vamsi Kadaru <54469793+vamsikadaru@users.noreply.github.com>
2025-12-11 13:54:33 -0500

    Use relative base path for deployment

--- vite.config.ts
+++ vite.config.ts
@@ -3,12 +3,12 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
-export default defineConfig(({ command }) => ({
-  base: command === 'build' ? "/vamsikadaru/" : "/",
+export default defineConfig({
+  base: "./",
   plugins: [react()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
-}))
+})
